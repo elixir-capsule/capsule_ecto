e90cc37e26821a379a5591059643af4526d270e2
T Floyd Wright <tfwright@gmail.com>
2020-08-20 19:17:12 -0500

    Add app skelly

---
+++ lib/capsule_ecto.ex
@@ -0,0 +1,18 @@
+defmodule CapsuleEcto do
+  @moduledoc """
+  Documentation for `CapsuleEcto`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> CapsuleEcto.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/capsule_ecto_test.exs
@@ -0,0 +1,8 @@
+defmodule CapsuleEctoTest do
+  use ExUnit.Case
+  doctest CapsuleEcto
+
+  test "greets the world" do
+    assert CapsuleEcto.hello() == :world
+  end
+end
