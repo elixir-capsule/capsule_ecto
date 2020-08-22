cfb8175a3088393bb02763a2b638394a4cdcee27
T Floyd Wright <tfwright@gmail.com>
2020-08-22 15:04:13 -0500

    Bump minor version

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule CapsuleEcto.MixProject do
   def project do
     [
       app: :capsule_ecto,
-      version: "0.1.0",
+      version: "0.2.0",
       description: "Ecto integration for Capsule",
       elixir: "~> 1.10",
       start_permanent: Mix.env() == :prod,
